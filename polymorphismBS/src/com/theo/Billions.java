f74b7d7c0fe0850135c48d1966c83139326de546
Theo <gmbaxevanos@gmx.ch>
2019-06-10 02:54:09 +0200

    poly

---
+++ polymorphismBS/src/com/theo/Billions.java
@@ -0,0 +1,13 @@
+package com.theo;
+
+public class Billions extends Serie {
+
+    public Billions() {
+        super("Billions");
+    }
+
+    @Override
+    public String plot(){
+        return "Money";
+    }
+}
---
+++ polymorphismBS/src/com/theo/BlackMirror.java
@@ -0,0 +1,13 @@
+package com.theo;
+
+public class BlackMirror extends Serie  {
+
+    public BlackMirror() {
+        super("BM");
+    }
+
+    @Override
+    public String plot(){
+        return "tech";
+    }
+}
