61a0287a03090d3ec16ac493ce02eaf86fe8b28b
Theo <gmbaxevanos@gmx.ch>
2019-07-26 16:50:21 +0200

    bs

---
+++ bindings2/src/sample/Controller.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+
+public class Controller {
+
+    @FXML
+    private StackPane pane;
+
+    @FXML
+    private Rectangle rectangle;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    public void initialize() {
+
+
+    }
+}
---
+++ bindings2/src/sample/Main.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        Scene scene = new Scene(root, 800, 600);
+        scene.getStylesheets().add(Main.class.getResource("styles.css").toExternalForm());
+
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
